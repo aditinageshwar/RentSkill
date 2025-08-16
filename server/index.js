750110b4e58b53210b5c5c81f1436ca04c1b3564
Aditi Nageshwar <aditinageshwar7@gmail.com>
2025-08-16 23:23:44 +0530

    deploy testing

--- server/index.js
+++ server/index.js
@@ -26,6 +26,8 @@ app.use(express.json());                                              //Middlewa
 app.use(express.urlencoded({ extended: true }));
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 app.use(cookieParser());
+app.set('trust proxy', 1);
+
 app.use(cors({
     origin: 'https://rentskill-2.onrender.com',                                   // Frontend URL
     credentials: true,
